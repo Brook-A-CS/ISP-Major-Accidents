e9284e5414b87e4a1b90d81d5f96cb2a2f8bf0ad
Rafid-A <rafid.ahmed@student.allenisd.org>
2022-04-19 19:54:11 -0500

    Basic AI Done & Some Prefabs

--- Refined Project Space/Assets/Scripts/EnemyShooting.cs
+++ Refined Project Space/Assets/Scripts/EnemyShooting.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour {
+public class EnemyShooting : MonoBehaviour {
     
-    public Transform GunSpot;
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
     public GameObject bulletPrefab;
-    public float fireDelay = 0.25f;
+    public float fireDelay = 0.50f;
     float cooldownTimer = 0;
 
     // Update is called once per frame
     void Update() {
         cooldownTimer -= Time.deltaTime;
 
-        if( Input.GetButton("Fire1") && cooldownTimer <= 0) {
+        if( cooldownTimer <= 0) {
             //Shoot
             //Debug.Log ("Pew!");
             cooldownTimer = fireDelay;
 
-            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, GunSpot.position, GunSpot.rotation);
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
             bulletGO.layer = gameObject.layer;
         }
     }
